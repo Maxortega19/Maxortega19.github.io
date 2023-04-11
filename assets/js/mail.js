edb4c5ea13cb8d3d14cdaf9a2f6be03073213250
Maxi Ortega <maxiortega@outlook.com>
2023-04-11 12:17:07 -0300

    Test Email.js

--- assets/js/mail.js
+++ assets/js/mail.js
@@ -1,24 +1,34 @@
-// // Este es solo un ejemplo de cómo puedes utilizar FormSubmit con JavaScript
+// contact.js
 
-// document.querySelector('form').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Prevenir el envío del formulario
-  
-//     // Realizar cualquier validación o manipulación de datos aquí
-  
-//     // Enviar el formulario a FormSubmit usando Fetch API
-//     fetch('https://formsubmit.co/maximilianocjortega@gmail.com', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify({ nombre: document.getElementById('name').value, apellido: document.getElementById('lastname').value, email: document.getElementById('email').value, mensaje: document.getElementById('message').value })
-//     })
-//     .then(function(response) {
-//       return response.json();
-//     })
-//     .then(function(data) {
-//       console.log(data); // Manejar la respuesta de FormSubmit
-//     })
-//     .catch(function(error) {
-//       console.error(error); // Manejar errores
-//     });
-//   });
-  
+// Configurar EmailJS con tu Public Key
+emailjs.init("_a9VNUBkPxFdE3ozg"); // Reemplaza TU_PUBLIC_KEY con tu Public Key de EmailJS
+
+// Manejar el envío del formulario
+document.getElementById("form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevenir el envío del formulario
+
+  // Obtener los valores del formulario
+  var name = document.getElementById("name").value;
+  var lastname = document.getElementById("lastname").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+
+  // Configurar los parámetros del correo
+  var params = {
+    name: name,
+    lastname: lastname,
+    email: email,
+    message: message
+  };
+
+  // Enviar el correo con EmailJS
+  emailjs.send("service_contactFormMaxi", "template_PortfolioForm", params) // Reemplaza TU_SERVICE_ID y TU_TEMPLATE_ID con tus IDs de servicio y plantilla de EmailJS
+    .then(function(response) {
+      console.log("Correo enviado con éxito", response);
+      // Aquí puedes agregar la lógica que deseas después de que se envíe el correo, por ejemplo, mostrar un mensaje de éxito en la página.
+    })
+    .catch(function(error) {
+      console.error("Error al enviar el correo", error);
+      // Aquí puedes agregar la lógica para manejar errores en el envío del correo, por ejemplo, mostrar un mensaje de error en la página.
+    });
+});
